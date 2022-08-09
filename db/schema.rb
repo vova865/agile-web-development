e1f26c0e400234f48cfc24f558f8b9d580a6fe56
Vladimir Ryshchenko <vr@rsdigital.ru>
2022-08-09 16:53:06 +0300

    c2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_05_140413) do
+ActiveRecord::Schema.define(version: 2022_08_08_151715) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "title"
+    t.integer "product_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_items_on_product_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -21,4 +29,5 @@ ActiveRecord::Schema.define(version: 2022_08_05_140413) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "items", "products"
 end
