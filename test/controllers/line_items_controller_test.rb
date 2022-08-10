1427183416171aa540f0a7c7fabff467c08c3254
Vladimir Ryshchenko <vr@rsdigital.ru>
2022-08-10 15:03:13 +0300

    e2

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -17,13 +17,11 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create line_item" do
     assert_difference('LineItem.count') do
-      post line_items_url, params: { product_id: products(:ruby).id  }
-      follow_redirect!
-
-      assert_select 'h2', 'Your Pragmatic Cart'
-      assert_select 'li', 'Programming Ruby 1.9'
+      post line_items_url, params: { product_id: products(:ruby).id }
     end
-
+    follow_redirect!
+    assert_select 'h2', 'Your Pragmatic Cart'
+    assert_select 'li', "1 \u00D7 Programming Ruby 1.9"
   end
 
   test "should show line_item" do
