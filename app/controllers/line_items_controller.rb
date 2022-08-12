fe3a22a2af531279871b82ced236c8f4cb6be0fa
Vladimir Ryshchenko <vr@rsdigital.ru>
2022-08-12 15:41:39 +0300

    f1

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -24,7 +24,7 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
+        format.html { redirect_to store_index_url }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -49,7 +49,7 @@ class LineItemsController < ApplicationController
     @line_item.destroy
 
     respond_to do |format|
-      format.html { redirect_to cart_path(session[:cart_id]), notice: "Line item was successfully destroyed." }
+      format.html { redirect_to store_index_url, notice: "Line item was successfully destroyed." }
       format.json { head :no_content }
     end
   end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,8 @@
 class StoreController < ApplicationController
   include CountView
+  include CurrentCart
   before_action :times_user_accessed_store, only: :index
+  before_action :set_cart
 
   def index
     @products = Product.order(:title)
