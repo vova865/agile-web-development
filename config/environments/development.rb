d980a41eed0f0436e608d2c7b8a5f932df933603
Vladimir Ryshchenko <vr@rsdigital.ru>
2022-08-17 10:52:04 +0300

    i1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,18 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "domain.of.sender.net",
+    authentication: "plain",
+    user_name: "dave",
+    password: "secret",
+    enable_starttls_auto: true
+  }
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
