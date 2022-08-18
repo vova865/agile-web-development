eb1cf2f554815b6fbfdee460cced368a36b26a16
Vladimir Ryshchenko <vr@rsdigital.ru>
2022-08-18 16:48:46 +0300

    j1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_15_090013) do
+ActiveRecord::Schema.define(version: 2022_08_17_134445) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -51,6 +51,13 @@ ActiveRecord::Schema.define(version: 2022_08_15_090013) do
     t.text "description"
     t.string "image_url"
     t.decimal "price", precision: 8, scale: 2
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
