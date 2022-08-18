06e946458980a5198d0f9888b106c5ea371f902f
Vladimir Ryshchenko <vr@rsdigital.ru>
2022-08-18 18:04:16 +0300

    j2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'admin', to: 'admin#index'
+  controller :sessions do
+    get 'sessions/new'
+    get 'sessions/create'
+    get 'sessions/destroy'
+  end
   resources :users
   resources :orders
   resources :line_items do
