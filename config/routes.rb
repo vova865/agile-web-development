80185479c5d79841664cdf17ab421821c220b00d
Vladimir <vova.com940@gmail.com>
2023-02-01 12:13:49 +0300

    chpter k

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,15 @@ Rails.application.routes.draw do
     delete 'logout', action: 'destroy'
   end
   resources :users
-  resources :orders
-  resources :line_items do
-    get :decrement, on: :member
-  end
-  resources :carts
-  root 'store#index', as: 'store_index'
   resources :products do
     get :who_bought, on: :member
+  end
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items do
+      get :decrement, on: :member
+    end
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
   end
 end
